31fdbebce6f04dc28085cd19e2a53b1519dd1436
Michael Nikitenko <xm4dn355x@gmail.com>
2021-02-04 20:12:28 +0500

    incapsulation

--- part1_lesson36.py
+++ part1_lesson36.py
@@ -25,10 +25,22 @@ class Person:
 
     def __init__(self, name: str):
         self.name = name
+        self._age = 20      # "Защищенное" свойство. Не надо трогать.
+        self.__tits = 3     # "Приватное" свойство. Питон защищает и не даёт его печатать и редактировать напрямую
 
     def print_info(self):
-        print(f'Hello, my name is {self.name}')
+        print(f'Hello, my name is {self.name}, Age: {self._age}')
 
+    @property
+    def tits(self):
+        return self.__tits
+
+    @tits.setter
+    def tits(self, size: int):
+        if size in range(1, 5):
+            self.__tits = size
+        else:
+            print('Wrong tits size')
 
 if __name__ == '__main__':
     a = A()
@@ -38,3 +50,10 @@ if __name__ == '__main__':
     print(a.say_hi(name='Buddy'))
     person = Person(name='Katy')
     person.print_info()
+    print(person._Person__tits)     # Достучались до приватного свойства
+    print(person.tits)
+    person.tits = -1
+    person.tits = 0
+    person.tits = 2
+    person.tits = 6
+    print(person.tits)
