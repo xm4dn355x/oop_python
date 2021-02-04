52bd81e9c57c36463c6b7468be1c08f937e6642e
Michael Nikitenko <xm4dn355x@gmail.com>
2021-02-04 16:21:27 +0500

    class constructor __init__

--- part1_lesson36.py
+++ part1_lesson36.py
@@ -20,9 +20,21 @@ class A:
             return f'Hi {self.name}'
 
 
+class Person:
+    anal = True
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def print_info(self):
+        print(f'Hello, my name is {self.name}')
+
+
 if __name__ == '__main__':
     a = A()
     print(a)
     print(a.property1)
     print(a.say_hi())
     print(a.say_hi(name='Buddy'))
+    person = Person(name='Katy')
+    person.print_info()
