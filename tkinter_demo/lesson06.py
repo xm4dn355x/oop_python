3f27630c79c0d437b1d10bdb05f88e6cdb91ad2b
Michael Nikitenko <xm4dn355x@gmail.com>
2021-02-16 17:12:50 +0500

    test palette without buttons list generator

---
+++ tkinter_demo/lesson06.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+########################################################################
+#                                                                      #
+#                                                                      #
+#                                                                      #
+# MIT License                                                          #
+# Copyright (c) 2021 Michael Nikitenko                                 #
+#                                                                      #
+########################################################################
+
+
+from tkinter import Tk, Button, Label, PhotoImage, Entry
+
+COLOR_OVERLAY_LIGHT = '#383B40'     # active clicked button
+COLOR_OVERLAY_DARK = '#2D2F34'      # foreground element
+COLOR_SURFACE = '#27292D'           # button, label or some other element
+COLOR_BASE = '#1F2023'              # Base element background
+COLOR_OVERFLOW = '#010101'          # all app background
+COLOR_TEXT = '#FFFFFF'              # Text color
+
+ROOT_WINDOW_WIDTH = 500
+ROOT_WINDOW_HEIGHT = 400
+ROOT_WONDOW_POS_X = 500
+ROOT_WINDOW_POS_Y = 500
+
+
+root = Tk()
+root.title('Hello GUI World!')
+root.iconbitmap('Python.ico')
+root.geometry(f'{ROOT_WINDOW_WIDTH}x{ROOT_WINDOW_HEIGHT}+{ROOT_WONDOW_POS_X}+{ROOT_WINDOW_POS_Y}')
+root.resizable(width=False, height=True)
+root.config(bg=COLOR_BASE)
+
+
+def change_color(color: str):
+    test_label['bg'] = color
+    test_entry.delete(0, 'end')
+    test_entry.insert(0, color)
+
+
+python_png = PhotoImage(file='python-powered-w-200x80.png')
+
+test_image_png = Label(
+    master=root,
+    image=python_png,
+    bg=COLOR_SURFACE,
+)
+test_image_png.pack()
+
+test_label = Label(
+    master=root,
+    text='Введите сообщение: ',
+    bg=COLOR_SURFACE,
+    fg=COLOR_TEXT,
+    height=1,
+    width=20,
+    justify='left',
+    anchor='center',
+    font=('Helvetica', 20, 'normal')
+)
+test_label.pack()
+
+test_entry = Entry(
+    master=root,
+    bg=COLOR_SURFACE,
+    fg=COLOR_TEXT,
+    width=22,
+    justify='left',
+    font=('Helvetica', 20, 'normal')
+)
+test_entry.insert(0, 'Hello')           # Вставляем текст по индексу
+test_entry.insert('end', ' World!!')    # Вставляем текст в конец
+print(test_entry.get())                 # Достаём текст из поля ввода
+test_entry.delete(first=0, last='end')  # Удаляем текст по индексам
+test_entry.pack()
+
+red_button = Button(
+    master=root,
+    text='Красный',
+    height=1,
+    width=20,
+    bg='red',
+    fg=COLOR_TEXT,
+    activebackground=COLOR_OVERLAY_LIGHT,
+    activeforeground=COLOR_TEXT,
+    font=('Helvetica', 20, 'normal'),
+    justify='center',
+    command=lambda: change_color('#FF0000')
+)
+red_button.pack()
+
+green_button = Button(
+    master=root,
+    text='Зеленый',
+    height=1,
+    width=20,
+    bg='green',
+    fg=COLOR_TEXT,
+    activebackground=COLOR_OVERLAY_LIGHT,
+    activeforeground=COLOR_TEXT,
+    font=('Helvetica', 20, 'normal'),
+    justify='center',
+    command=lambda: change_color('#00FF00')
+)
+green_button.pack()
+
+blue_button = Button(
+    master=root,
+    text='Голубой',
+    height=1,
+    width=20,
+    bg='blue',
+    fg=COLOR_TEXT,
+    activebackground=COLOR_OVERLAY_LIGHT,
+    activeforeground=COLOR_TEXT,
+    font=('Helvetica', 20, 'normal'),
+    justify='center',
+    command=lambda: change_color('#0000FF')
+)
+blue_button.pack()
+
+
+if __name__ == '__main__':
+    root.mainloop()
