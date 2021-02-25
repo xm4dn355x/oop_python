0f975cc55ee278d3de4587c40ab3d4c075659658
Michael Nikitenko <xm4dn355x@gmail.com>
2021-02-25 11:57:43 +0500

    dark theme color schema exported to module

---
+++ tkinter_demo/dark_theme_colors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+########################################################################
+#                                                                      #
+#                                                                      #
+#                                                                      #
+# MIT License                                                          #
+# Copyright (c) 2021 Michael Nikitenko                                 #
+#                                                                      #
+########################################################################
+
+
+COLOR_OVERLAY_LIGHT = '#383B40'  # active clicked button
+COLOR_OVERLAY_DARK = '#2D2F34'  # foreground element
+COLOR_SURFACE = '#27292D'  # button, label or some other element
+COLOR_BASE = '#1F2023'  # Base element background
+COLOR_OVERFLOW = '#010101'  # all app background
+
+COLOR_TEXT = '#FFFFFF'  # Text color
